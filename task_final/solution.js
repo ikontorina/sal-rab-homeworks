4e779a79416050c46cbfcd104c0a95de7fd167ed
ikontorina <iokontorina@gmail.com>
2021-12-18 20:47:11 +0300

    Task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,24 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    
+    let data = {client: "", order: {address, sum}, goods: []};
 
+    data.client = name + " " + phone;
+    
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address. flat;
+    data.order.sum = sum;
+    
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let product = {title: goods.title, count: goods.count}
+
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let product = {title: goods[i].title, count: goods[i].count};
+        data.goods.push(product);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
